aac66077ba555c32dd5165c74904843c8f53ca90
Richard Lieknis <richard.lieknis@web.de>
2023-04-06 19:54:47 +0200

    menu overlay added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translate(0px, -30px)'}),
+        animate('1250ms', style({opacity: 1, transform: 'translate(0, 0)'}))
+      ])]
+    ),
+  ]
+  
 })
 export class HeaderComponent {
+  isMenuOpen = true;
+  isPlaying = false;
+  menuImage = 'assets/img/menu1.png';
+  images = [
+    'assets/img/menu1.png',
+    'assets/img/menu2.png',
+    'assets/img/menu3.png',
+    'assets/img/menu4.png',
+    'assets/img/menu5.png'];
 
+  toggleMenu() {
+    if (this.isMenuOpen) {
+      this.isPlaying = false;
+      setTimeout(() => this.isMenuOpen = false, 500);
+    } else {
+      this.isMenuOpen = true;
+      this.isPlaying = true;
+    }
+  }
 }
