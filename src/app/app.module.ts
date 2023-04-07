07f5d3b3c909dd493c5828085b32a10f071bc066
Richard Lieknis <richard.lieknis@web.de>
2023-04-07 13:27:47 +0200

    about me section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule,
