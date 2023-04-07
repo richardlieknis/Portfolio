7e1a14e6aeb66326be111c1ab424a7609a025d7b
Richard Lieknis <richard.lieknis@web.de>
2023-04-07 23:04:12 +0200

    myskills section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
+import { MyskillsComponent } from './myskills/myskills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutmeComponent
+    AboutmeComponent,
+    MyskillsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/myskills/myskills.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myskills',
+  templateUrl: './myskills.component.html',
+  styleUrls: ['./myskills.component.scss']
+})
+export class MyskillsComponent {
+
+}
