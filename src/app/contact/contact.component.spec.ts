136542ec7f4c93e003d74c0ca949b08add04a5c3
Richard Lieknis <richard.lieknis@web.de>
2023-04-19 20:51:25 +0200

    some adjustments in contact-form

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ContactComponent } from './contact.component';
 
 describe('ContactComponent', () => {
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-
-
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
